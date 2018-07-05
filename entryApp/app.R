583a24a8a7c8815b06b2dfdf98ffb021375e7a77
oshah97 <oshah97@outlook.com>
2018-07-05 13:57:18 -0700

    radio button update, intern list update, current time button, email vs in person interaction

--- entryApp/app.R
+++ entryApp/app.R
@@ -1,90 +1,107 @@
 #variables
 
-internList <- scan("internList.csv", sep=",", what=list('character'))
-staffList <- read.table("isbAllStaff",header=FALSE, sep="\t")
+#internList <- scan("internList.csv", sep=",", what=list('character'))
+internList <- read.table("internList.csv", sep="\t", header=FALSE, fill=TRUE) #to add blank above list of names
+staffList <- read.table("isbAllStaff",header=FALSE, sep="\t", fill=TRUE)
 dataDir <- "data" #directory for data
 newLine <- ""
 
 library(shiny)
 library(shinyTime)
 
-ui <- fluidPage(headerPanel(
-                tags$h2("Enter a new interaction"), windowTitle = "ISB Interactions"
-                ),
-                sidebarLayout(
-                  sidebarPanel(
-                    dateInput('date',
-                              label = 'Date:',
-                              value = Sys.Date()),
-                    selectInput("name", "Who are you?",
-                                choices = internList[[1]]),
-                    selectInput("partner", "Who did you interact with?",
-                                choices = staffList[[1]]),
-                    selectInput("type", "Interaction Type:",
-                                c("Administrative" = "adm",
-                                  "Programming" = "prg",
-                                  "Data" = "data",
-                                  "Biology" = "bio",
-                                  "Social" = "soc")),
-                    timeInput("time", "Time:", value = Sys.time(), seconds=FALSE),
-                    sliderInput("dur", "Duration (minutes):", min = 1, max = 120, value = 2),
-                    actionButton(inputId = "submit",
-                                 label = "Submit"),
-                    tags$br(),
-                    textOutput("count"),
-   			#img(src="isb.png", height=20, width=20),
-
-                    tableOutput("data")
-                  ),
-                  mainPanel(
-                    tabsetPanel(
-		        tabPanel("Project Summary", includeHTML("projectSummary.html")),
-			tabPanel("Your Existing Network", "Your network will display here"),
+ui <- fluidPage(
+    tags$head(tags$style(".rightAlign{float:right;}")),
+    headerPanel(
+    tags$h2("Enter a new interaction"), windowTitle = "ISB Interactions"
+    ),
+    sidebarLayout(
+        sidebarPanel(
+            dateInput('date',
+                      label = 'Date:',
+                      value = NULL,
+                      min = "2018-06-01",
+                      max = "2018-08-31"),
+            selectInput("name", "Who are you?",
+                        choices = internList[[1]]),
+            selectInput("partner", "Who did you interact with?",
+                        choices = staffList[[1]]),
+                                           
+            selectInput("type", "Interaction Type:",
+                        c("Administrative" = "adm",
+                          "Programming" = "prg",
+                          "Data" = "data",
+                          "Biology" = "bio",
+                          "Social" = "soc")),
+            radioButtons("type2", "Was it...?",
+               c("In Person" = "norm",
+                 "Email" = "email")),
+            actionButton("to_current_time", "Current time", class = 'rightAlign'),
+            timeInput("time", "Time:", seconds=FALSE),
+            #textOutput("sysTime"),
+            sliderInput("dur", "Duration (minutes):", min = 1, max = 120, value = 2),
+            textOutput("count"),
+            actionButton(inputId = "submit",
+                         label = "Submit")
+            ),
+        mainPanel(
+            tabsetPanel(
+                tabPanel("Project Summary", includeHTML("projectSummary.html")),
+                tabPanel("Your Existing Network", "Your network will display here"),
                                         #img(src="network.png", height=450, width=750),
-        		tabPanel("Tabular View", tableOutput("table"))
-      			)
-                  )
-                )
-                )
+                tabPanel("Tabular View", tableOutput("table"))
+            )
+        )
+    )
+)
 
 
-server <- function(input,output) {
+server <- function(input,output, session) {
 
-    subCounter <- reactiveValues(countervalue = 0)
-
-    observeEvent(input$submit, {
-      subCounter$countervalue <- subCounter$countervalue + 1
+    observeEvent(input$to_current_time, {
+        updateTimeInput(session, "time", value = Sys.time())
     })
 
+    output$sysTime <- renderText({
+        paste(Sys.time())
+    })
+    
+    subCounter <- reactiveValues(countervalue = 0)
+    
     output$count <- renderText({
         if(subCounter$countervalue == 1) {
             paste("You've submitted ", subCounter$countervalue, " interaction!")
         } else {
             paste("You've submitted ", subCounter$countervalue, " interactions!")
-            }
+        }
     })
+    
+    observeEvent(input$submit, {newLine <- data.frame("date" = as.character.Date(input$date),
+                                                      "a" = input$name,
+                                                      "b" = input$partner,
+                                                      "type" = input$type,
+                                                      "startTime" = as.character.Date(input$time),
+                                                      "duration" = input$dur,
+                                                      stringsAsFactors = FALSE)
 
-  observeEvent(input$submit, {newLine <- data.frame("date" = as.character.Date(input$date),
-    		      		"a" = input$name,
-				"b" = input$partner,
-				"type" = input$type,
-				"startTime" = as.character.Date(input$time),
-				"duration" = input$dur,
-				stringsAsFactors = FALSE)
+                                                      if(input$type2 == "norm"){
+                                                          filename <- sprintf("%s/interaction-%s-%s.RData", dataDir,input$name,Sys.time())
+                                                      } else {
+                                                          filename <- sprintf("%s/emailInteraction-%s-%s.RData", dataDir,input$name,Sys.time())
+                                                      }
+                                                      
+                                                      filename <- gsub(" ", "", filename, fixed = TRUE)
+                                                      save(newLine, file = filename)
 
-        		filename <- sprintf("%s/interaction-%s-%s.RData", dataDir,input$name,Sys.time())
-			filename <- gsub(" ", "", filename, fixed = TRUE)
-                        save(newLine, file = filename)
-
-                        print("Interaciton Logged.")
-  })
+                                                      subCounter$countervalue <- subCounter$countervalue + 1
+                                                      print("Interaciton Logged.")
+    })
 }
 
 shinyOptions <- list()
 if(Sys.info()[["nodename"]] == "trena.systemsbiology.net"){
-   print("running on trena, using port 60013")
-   shinyOptions <- list(host="0.0.0.0", port=60013, launch.browser=FALSE)
-   }
+    print("running on trena, using port 60013")
+    shinyOptions <- list(host="0.0.0.0", port=60013, launch.browser=FALSE)
+}
 
 app <- shinyApp(ui=ui,server=server, options=shinyOptions)
 
