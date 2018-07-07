842a0093c6386137afe36a4e6b09676d34b518a4
oshah97 <oshah97@outlook.com>
2018-07-06 17:52:27 -0700

    function to load data into table and tests to confirm success

--- entryApp/dirToTable.R
+++ entryApp/dirToTable.R
@@ -1,18 +1,46 @@
-
-
-
-
-
-
-files <- grep(".RData", list.files("."), value=TRUE)
+library(RUnit)
 
 tbl.master <- data.frame()
-for(file in head(files)){
-   load(file)
-   tbl <- newLine
-   if(ncol(tbl) == 6)
-       tbl$mode <- "inPerson"
-   stopifnot(colnames(tbl) == c("date", "a", "b", "type", "startTime", "duration", "mode"))
-   printf("%s: %d, %d", file, nrow(tbl), ncol(tbl))
-   tbl.master <- rbind(tbl.master, tbl)
-   }
+#--------------------------------------------------------------------------------#
+runTests <- function()
+{
+    test_dirToTable()
+
+} # runTests
+#--------------------------------------------------------------------------------#
+dirToTable <- function(dir, quiet=TRUE)
+{
+   if(!file.exists(dir))
+       stop(sprintf("The directory %s does not exist.", dir))
+   files <- grep(".RData", list.files(dir), value=TRUE)
+
+   for(file in files){
+       load(file.path(dir,file))
+       tbl <- newLine
+       if(!quiet)
+           print(tbl)
+       if(ncol(tbl) == 6)
+           tbl$mode <- "inPerson"
+       stopifnot(colnames(tbl) == c("date", "a", "b", "type", "startTime", "duration", "mode"))
+       tbl.master <- rbind(tbl.master, tbl)
+   } # for 
+    
+   return(tbl.master)
+   
+} # dirToTable
+#--------------------------------------------------------------------------------#
+test_dirToTable <- function()
+{
+    printf("--- test_dirToTable")
+    
+    dir <- "data"
+    tbl <- dirToTable(dir)
+    checkTrue(is.data.frame(tbl))
+    checkEquals(ncol(tbl), 7)
+    
+}# test_dirToTable
+#--------------------------------------------------------------------------------#
+
+
+
+
