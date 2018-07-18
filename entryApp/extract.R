958fb49c691e6527c71fa33cd1825eb97dc36b6b
oshah97 <oshah97@outlook.com>
2018-07-18 12:05:14 -0700

    cosmetic

--- entryApp/extract.R
+++ entryApp/extract.R
@@ -9,15 +9,14 @@ file.name <- dir(path, pattern = ".RData")
 print(file.name)
 print(extract)
 print(length(file.name))
-print(load("interaction-AishahMohamed-2018-07-1009:56:42.RData"))
 
 extract <- function()
 {
     for(i in 1:length(file.name)) {
         
         print(load(file.name[i]))
-                                        #file.unload <- print(load(file.name))
-                                        #file <- read.table(load(file.name[i]), header=TRUE, sep="\t", stringsAsFactors=FALSE)
+        #file.unload <- print(load(file.name))
+        #file <- read.table(load(file.name[i]), header=TRUE, sep="\t", stringsAsFactors=FALSE)
         extract <- rbind(extract, newLine)
     }
     print(head(extract))
